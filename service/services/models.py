08af22b080f96e79d9ecdf73df1b6008aff4a23c
Vadym Koroliuk <vkqa@outlook.com>
2023-01-28 09:52:22 -0800

    Day 2-3 commit

--- service/services/models.py
+++ service/services/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
+
+from client.models import Client
+
 
 # Create your models here.
+class Service(models.Model):
+    name = models.CharField(max_length=50)
+    full_price = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return f"{self.name} service"
+
+
+class Plan(models.Model):
+    PLAN_TYPES = (
+        ('full', 'Full'),
+        ('student', 'Student'),
+        ('discount', 'Discount')
+    )
+
+    plan_type = models.CharField(choices=PLAN_TYPES, max_length=10)
+    discount_percent = models.PositiveSmallIntegerField(default=0, validators=[MaxValueValidator(100)])
+
+    def __str__(self):
+        return f"Plan {self.plan_type}"
+
+
+class Subscription(models.Model):
+    client = models.ForeignKey(Client, related_name='subscription', on_delete=models.PROTECT)
+    service = models.ForeignKey(Service, related_name='subscription', on_delete=models.PROTECT)
+    plan = models.ForeignKey(Plan, related_name='subscription', on_delete=models.PROTECT)
