0b0eeaece27cfbb160edcfa9ed5d06b33a939868
Vadym Koroliuk <vkqa@outlook.com>
2023-01-29 16:05:55 -0800

    Day 5-2 commit

--- service/celery_app.py
+++ service/celery_app.py
@@ -1,4 +1,6 @@
+
 import os
+import time
 
 from celery import Celery
 from django.conf import settings
@@ -7,5 +9,11 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'service.settings')
 
 app = Celery('service')
 app.config_from_object('django.conf:settings')
-app.conf.broker_url = settings.CELERY_BROCER_URL
+app.conf.broker_url = settings.CELERY_BROKER_URL
 app.autodiscover_tasks()
+
+
+@app.task()
+def debug_task():
+    time.sleep(20)
+    print('Hello form debug_task')
--- service/service/settings.py
+++ service/service/settings.py
@@ -13,10 +13,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -28,7 +26,6 @@ SECRET_KEY = 'django-insecure-hoic_3_n_u*m6&knp3(1$uoa^#$*j=d0aj#2@x1$$h+^!83q3y
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -73,7 +70,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'service.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -95,7 +91,6 @@ DATABASES = {
 
 }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
@@ -114,7 +109,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -127,7 +121,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
@@ -143,7 +136,7 @@ LOGGING = {
     'version': 1,
     'handlers': {
         'console': {'class': 'logging.StreamHandler'}
-        },
+    },
     'loggers': {
         'django.db.backends': {
             'handlers': ['console'],
@@ -151,3 +144,5 @@ LOGGING = {
         }
     }
 }
+
+CELERY_BROKER_URL = 'redis://redis:6379/0'
